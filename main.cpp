d52292330ab96867637d7a8de0e8803cb5bfc3ae
Jasmine <jazzyysuarez@gmail.com>
2025-09-14 16:52:04 -0700

    Initial commit, will take break

---
+++ main.cpp
@@ -0,0 +1,18 @@
+// COMSC-210 | Lab 6 | Jasmine Suarez
+// IDE used: VS Code
+
+#include <iostream>
+using namespace std;
+
+const int SIZE = 5;
+
+int main() {
+    double *arr = nullptr;
+    arr = new double[SIZE];
+
+
+
+    delete [] arr;
+
+    return 0;
+}
