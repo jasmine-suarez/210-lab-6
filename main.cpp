77647773d73ded992ca10b602c486be0966508a9
Jasmine <jazzyysuarez@gmail.com>
2025-09-14 17:31:37 -0700

    made enterArrayData()

--- main.cpp
+++ main.cpp
@@ -6,13 +6,22 @@ using namespace std;
 
 const int SIZE = 5;
 
+void enterArrayData(double *arr, int SIZE);
+
 int main() {
     double *arr = nullptr;
     arr = new double[SIZE];
 
-
+    enterArrayData(arr, SIZE);
 
     delete [] arr;
 
     return 0;
+}
+
+void enterArrayData(double *arr, int SIZE) {
+    cout << "Enter " << SIZE << " values:" << endl;
+    for (int i = 0; i < SIZE; i++) {
+        cin >> *(arr + i);
+    }
 }
