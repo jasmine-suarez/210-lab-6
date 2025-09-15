432924f53c3c3012cd424ec405879a3209dc98c4
Jasmine <jazzyysuarez@gmail.com>
2025-09-14 17:40:09 -0700

    wrote outputArrayData()

--- main.cpp
+++ main.cpp
@@ -7,12 +7,14 @@ using namespace std;
 const int SIZE = 5;
 
 void enterArrayData(double *arr, int SIZE);
+void outputArrayData(double *arr, int SIZE);
 
 int main() {
     double *arr = nullptr;
     arr = new double[SIZE];
 
     enterArrayData(arr, SIZE);
+    outputArrayData(arr, SIZE);
 
     delete [] arr;
 
@@ -23,5 +25,12 @@ void enterArrayData(double *arr, int SIZE) {
     cout << "Enter " << SIZE << " values:" << endl;
     for (int i = 0; i < SIZE; i++) {
         cin >> *(arr + i);
+    }
+}
+
+void outputArrayData(double *arr, int SIZE) {
+    cout << "Array elements: ";
+    for (int i = 0; i < SIZE; i++){
+        cout << *(arr + i) << " ";
     }
 }
